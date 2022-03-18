ed3ead5f8b736b9096322ece4c56b7ccd6978e6b
Neşe İLHAN <neseilhannn@gmail.com>
2022-03-18 14:54:35 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
-// import Categories from './Categories';
+import Categories from './Categories';
 import items from './data';
-
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  // const [Categories, setCategories] = useState([]);
-  return (
-<main>
-  <section className='menu section'>
-    <div className='title'>
-      <h2>
-        our menu
-      </h2>
-    <div className='underline'></div>
-    </div>
-    {/* <Categories/> */}
-    <Menu items={menuItems}/>
+  const [categories, setCategories] = useState(allCategories);
 
-  </section>
-</main>
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
