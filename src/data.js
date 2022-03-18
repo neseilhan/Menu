39110e170e90a4e52b9f0697d36a6e7926b1285a
Neşe İLHAN <neseilhannn@gmail.com>
2022-03-18 14:54:22 +0300

    .

---
+++ src/data.js
@@ -0,0 +1,82 @@
+const menu= [
+    {
+        id: 1,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.99,
+        img: './images/pancake.jpg',
+        desc: "I'm baby woke milkshake wolf bitters live-edge blue bottle, hammock ffreegan copper mug whatever cold-pressed"
+    },
+    {
+        id: 2,
+        title: "diner double",
+        category: 'lunch',
+        price: 13.99,
+        img: './images/diner.jpg',
+        desc: "vaporware iphone numblecore selvage raw denim slow carb leggings gochujang helvetice man braid jianbing, Marfa thundercats",
+   
+    },
+    {
+        id: 3,
+        title: "godzilla milkshake",
+        category: 'shakes',
+        price: 6.99,
+        img: './images/milkshake.jpg',
+        desc: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+    },
+    {
+        id: 4,
+        title: "country delight",
+        category: 'breakfast',
+        price: 20.99,
+        img: './images/breakfast.jpg',
+        desc: "shabby chic keffiye neutra snackwave pork belly shoreditch. Prism austin milkshk truffaut.", 
+    },
+    {
+        id: 5,
+        title: "egg attack",
+        category:"lunch",
+        price: 22.99,
+        img: './images/egg.jpg',
+        desc: "franzen vegan pabst bicycle right kickstarter pinterest meditation farm-to-table 90's pop-up",
+   
+    },
+    {
+        id: 6,
+        title:"oreo dream",
+        category:"shakes",
+        price: 18.99,
+        img:'./images/oreod.jpg',
+        desc: "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
+   
+    },
+   
+    {
+       id: 7,
+       title: "bacon overflow",
+       category:"breakfast",
+       price: 8.99,
+       img:'./images/bacon.jpg',
+       desc:"carry jianbing normcore freegan. Viral single-origin coffe live-edge, pork belly cloud bread iceland put a bird",
+   
+    },
+    {
+        id:8,
+        title: "american classic",
+        category:'lunch',
+        price: 12.99,
+        img: './images/classic.jpg',
+        desc: "on it tumblr kickstarter thundercats migas everyday carry siwuid palo santo leggings. Food truck truffaut.",
+   
+    },
+    {
+        id:9,
+        title: 'quarantine buddy',
+        category: 'shakes',
+        price: 16.99,
+        img: './images/buddy.jpg',
+        desc: "skateboard fam synth authantic semiotics, live-edge lyft af, edison bulb yuccie crufix microdosing.",
+   
+    },
+   ];
+   export default menu;
